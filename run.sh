29aa65951fcb87dddeab61f890b1f24c2dd89901
rizo928 <rizo928@users.noreply.github.com>
2017-04-28 14:22:15 -0400

    Add ability to run user scripts

--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+# execute any pre-exec scripts, useful for images
+# based on this image
+for i in /scripts/*sh
+do
+    if [ -e "${i}" ]; then
+        echo "[i] user script - processing $i"
+        . "${i}"
+    fi
+done
+
 # set apache as owner/group
 if [ "$FIX_OWNERSHIP" != "" ]; then
 	chown -R apache:apache /app
